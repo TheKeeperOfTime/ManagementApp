b899b7096c6da288a3df0f7bb00aa3b688e13b7c
Spencer Haggard <spencer.timekeeper99@gmail.com>
2016-11-01 11:23:44 -0400

    Initial Commit

---
+++ src/main/java/org/elevenfifty/managementapp/controller/IndexController.java
@@ -0,0 +1,17 @@
+package org.elevenfifty.managementapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/Welcome")
+	public String welcome(Model model) {
+		return "welcome";
+	}
+		
+	
+	
+}
